e2671a87f9e321a558f2742ea3ad8d8d5c4673ff
Antonio Giganti <antoniogiganti@Antonios-MacBook-Pro.local>
2020-08-19 16:37:15 +0200

    added midi keyboard compatibility for visualization + main menu page

--- script/main.js
+++ script/main.js
@@ -172,3 +172,182 @@ document.addEventListener('keyup', function(event){
         pattern.getArrayElement(15).isActive = false;
     }
 } );
+
+
+
+/*        :::::::::::            MIDI FUNCTIONALITIES            :::::::::::        */
+
+if (navigator.requestMIDIAccess) {
+    console.log('This browser supports WebMIDI!');
+} else {
+    console.log('WebMIDI is not supported in this browser.');
+}
+
+navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+function onMIDIFailure() {
+    console.log('Could not access any MIDI device.');
+}
+
+function onMIDISuccess(midiAccess) {
+    for (var input of midiAccess.inputs.values()){
+        input.onmidimessage = getMIDIMessage;
+    }
+}
+
+function getMIDIMessage(message){
+    var command = message.data[0];
+    var note = message.data[1];
+    var velocity = (message.data.length > 2) ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
+
+    console.log("Command: " + command + "; Note: " + note + "; Velocity: " + velocity);
+
+    switch (command) {
+        case 144: // noteOn
+            if (velocity > 0) {
+                noteOn(note, velocity);
+            } else {
+                noteOff(note);
+            }
+            break;
+        case 128: // noteOff
+            noteOff(note);
+            break;
+        // we could easily expand this switch statement to cover other types of commands such as controllers or sysex
+    }
+}
+
+function noteOn(note, velocity){
+    activateNoteOnGrid(note);
+    pattern.redrawRequired = true; //in order to trigger the redraw function
+}
+
+function noteOff(note){
+    deactivateNoteOnGrid(note);
+    pattern.redrawRequired = true; //in order to trigger the redraw function
+}
+
+function activateNoteOnGrid(note){
+    var noteName = note % 12;
+
+    switch(noteName){
+        case 0: //C
+            pattern.getArrayElement(0).isActive = true;
+            pattern.getArrayElement(14).isActive = true;
+            pattern.getArrayElement(15).isActive = true;
+        break;
+
+        case 1: //C#
+            pattern.getArrayElement(8).isActive = true;
+        break;
+
+        case 2: //D
+            pattern.getArrayElement(2).isActive = true;
+            pattern.getArrayElement(17).isActive = true;
+        break;
+
+        case 3: //D#
+            pattern.getArrayElement(11).isActive = true;
+        break;
+
+        case 4: //E
+            pattern.getArrayElement(4).isActive = true;
+            pattern.getArrayElement(5).isActive = true;
+            pattern.getArrayElement(19).isActive = true;
+        break;
+
+        case 5: //F
+            pattern.getArrayElement(13).isActive = true;
+        break;
+
+        case 6: //F#
+            pattern.getArrayElement(7).isActive = true;
+        break;
+
+        case 7: //G
+            pattern.getArrayElement(1).isActive = true;
+            pattern.getArrayElement(16).isActive = true;
+        break;
+
+        case 8: //G#
+            pattern.getArrayElement(9).isActive = true;
+            pattern.getArrayElement(10).isActive = true;
+        break;
+
+        case 9: //A
+            pattern.getArrayElement(3).isActive = true;
+            pattern.getArrayElement(18).isActive = true;
+        break;
+
+        case 10: //A#
+            pattern.getArrayElement(12).isActive = true;
+        break;
+
+        case 11: //B
+            pattern.getArrayElement(6).isActive = true;
+        break;
+
+    }
+}
+
+function deactivateNoteOnGrid(note){
+    var noteName = note % 12;
+
+    switch(noteName){
+        case 0: //C
+            pattern.getArrayElement(0).isActive = false;
+            pattern.getArrayElement(14).isActive = false;
+            pattern.getArrayElement(15).isActive = false;
+        break;
+
+        case 1: //C#
+            pattern.getArrayElement(8).isActive = false;
+        break;
+
+        case 2: //D
+            pattern.getArrayElement(2).isActive = false;
+            pattern.getArrayElement(17).isActive = false;
+        break;
+
+        case 3: //D#
+            pattern.getArrayElement(11).isActive = false;
+        break;
+
+        case 4: //E
+            pattern.getArrayElement(4).isActive = false;
+            pattern.getArrayElement(5).isActive = false;
+            pattern.getArrayElement(19).isActive = false;
+        break;
+
+        case 5: //F
+            pattern.getArrayElement(13).isActive = false;
+        break;
+
+        case 6: //F#
+            pattern.getArrayElement(7).isActive = false;
+        break;
+
+        case 7: //G
+            pattern.getArrayElement(1).isActive = false;
+            pattern.getArrayElement(16).isActive = false;
+        break;
+
+        case 8: //G#
+            pattern.getArrayElement(9).isActive = false;
+            pattern.getArrayElement(10).isActive = false;
+        break;
+
+        case 9: //A
+            pattern.getArrayElement(3).isActive = false;
+            pattern.getArrayElement(18).isActive = false;
+        break;
+
+        case 10: //A#
+            pattern.getArrayElement(12).isActive = false;
+        break;
+
+        case 11: //B
+            pattern.getArrayElement(6).isActive = false;
+        break;
+    }
+}
